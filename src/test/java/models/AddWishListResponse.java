6ae05144c8899d0554c135093f548f6d5a2a310d
PodlesnyySergey <serpodl@gmail.com>
2021-02-27 16:42:23 +0300

    Add test with add in WishList.

---
+++ src/test/java/models/AddWishListResponse.java
@@ -0,0 +1,69 @@
+package models;
+
+import java.util.HashMap;
+import java.util.Map;
+import com.fasterxml.jackson.annotation.JsonAnyGetter;
+import com.fasterxml.jackson.annotation.JsonAnySetter;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@JsonPropertyOrder({
+        "success",
+        "message",
+        "updatetopwishlistsectionhtml"
+})
+public class AddWishListResponse {
+
+    @JsonProperty("success")
+    private Boolean success;
+    @JsonProperty("message")
+    private String message;
+    @JsonProperty("updatetopwishlistsectionhtml")
+    private String updatetopwishlistsectionhtml;
+    @JsonIgnore
+    private Map<String, Object> additionalProperties = new HashMap<String, Object>();
+
+    @JsonProperty("success")
+    public Boolean getSuccess() {
+        return success;
+    }
+
+    @JsonProperty("success")
+    public void setSuccess(Boolean success) {
+        this.success = success;
+    }
+
+    @JsonProperty("message")
+    public String getMessage() {
+        return message;
+    }
+
+    @JsonProperty("message")
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    @JsonProperty("updatetopwishlistsectionhtml")
+    public String getUpdatetopwishlistsectionhtml() {
+        return updatetopwishlistsectionhtml;
+    }
+
+    @JsonProperty("updatetopwishlistsectionhtml")
+    public void setUpdatetopwishlistsectionhtml(String updatetopwishlistsectionhtml) {
+        this.updatetopwishlistsectionhtml = updatetopwishlistsectionhtml;
+    }
+
+    @JsonAnyGetter
+    public Map<String, Object> getAdditionalProperties() {
+        return this.additionalProperties;
+    }
+
+    @JsonAnySetter
+    public void setAdditionalProperty(String name, Object value) {
+        this.additionalProperties.put(name, value);
+    }
+
+}
